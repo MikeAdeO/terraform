20193d09e7c6966cab88b981c9219dc784cd5772
MikeAdeO <56716012+MikeAdeO@users.noreply.github.com>
2022-08-26 13:51:34 +0100

    deploy VPC and subnet

--- main.tf
+++ main.tf
@@ -4,11 +4,33 @@ provider "aws" {
   shared_credentials_files = ["/Users/USER/.aws/credentials"]
 }
 
-resource "aws_instance" "my-first-server" {
-  ami           = "ami-052efd3df9dad4825"
-  instance_type = "t2.micro"
+# AWS EC2 INSTANCE
+# resource "aws_instance" "my-first-server" {
+#   ami           = "ami-052efd3df9dad4825"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "Ubuntu"
+#   }
+# }
+
+# AWS VPC
+resource "aws_vpc" "first-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    "Name" = "production"
+  }
+  
+}
+
+# AWS SUBNET 1
+resource "aws_subnet" "subnet-1" {
+  vpc_id = aws_vpc.first-vpc.id
+  cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "Ubuntu"
+    "Name" = "prod-subnet-1"
   }
 }
+
+
